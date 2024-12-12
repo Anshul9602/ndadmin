a36dcb4d21b460a699fcb1a9b4eedf4dc6c923a6
Anshul9602 <anshulkumar242507@gmail.com>
2024-12-12 15:07:58 +0530

    updated role listing

--- app/Views/admin/userroll/role_form.php
+++ app/Views/admin/userroll/role_form.php
@@ -1,4 +1,6 @@
 <?= $this->include('admin/common/header') ?>
+
+
 <div class="content-body">
 
     <div class="container-fluid">
@@ -15,11 +17,11 @@
                 </div>
             </div>
             <div class="col-sm-6 justify-content-sm-end mt-2 mt-sm-0 d-flex">
-               
-                    <button class="btn btn-primary btn-rounded"id="save_btn1">
-                        Save
-                    </button>
-              &nbsp;
+
+                <button class="btn btn-primary btn-rounded" id="save_btn1">
+                    Save
+                </button>
+                &nbsp;
                 <button class="btn btn-danger btn-rounded">
                     <li class="fa fa-arrow-left"></li>
                 </button>
@@ -27,43 +29,48 @@
             </div>
         </div>
         <!-- row -->
+        <style>
+            #persel .bootstrap-select{
+                width: 100%;
+            }
+        </style>
         <div class="row">
             <div class="col-12">
                 <div class="card">
                     <div class="card-body">
-                        <form action="<?php echo base_url('admin/role/role_form/'. $token); ?>" method="post">
+                        <form action="<?php echo base_url('admin/role/role_form/' . $token); ?>" method="post">
                             <div class="custom-tab-1">
                                 <ul class="nav nav-tabs">
                                     <li class="nav-item">
                                         <a class="nav-link active" data-toggle="tab" href="#home1"><i class="la la-home mr-2"></i> General</a>
                                     </li>
-                                   
+
                                 </ul>
                                 <div class="tab-content">
                                     <div class="tab-pane fade show active" id="home1" role="tabpanel">
                                         <div class="row pt-2">
-                                            <div class="col-sm-4">
-                                                <input type="text" class="form-control" name="name" placeholder="Category name">
+                                            <div class="col-sm-5 mt-3">
+                                            <label for="workSelection"style="color:#000;">Name Of Role</label>
+                                                <input type="text" class="form-control" name="name" placeholder="Enter role">
                                             </div>
-                                            <div class="col-sm-4">
-                                                <input type="text" class="form-control" name="order" placeholder="Sort Order">
+                                            <div class="col-sm-5 mt-3"id="persel">
+                                                <label for="workSelection"style="color:#000;">Select Permission:</label>
+                                                <br>
+                                                <select id="workSelection" name="work[]" multiple style="width: 100%;">
+                                                    <option value="edit">Edit</option>
+                                                    <option value="delete">Delete</option>
+                                                    <option value="update">Update</option>
+                                                    <option value="view">View</option>
+                                                </select>
                                             </div>
-                                            <div class="col-sm-4">
+                                            <div class="col-sm-5 mt-3">
+                                            <label for="workSelection" style="color:#000;">Status</label>
                                                 <select class="form-control form-control-lg" name="status">
                                                     <option value="Enable">Enable</option>
                                                     <option value="Disable">Disable</option>
                                                 </select>
                                             </div>
                                         </div>
-                                        <div class="row">
-                                            <div class="col-12 pt-4">
-                                                <p class="mb-0">Category Description</p>
-                                                <div class="summernote pt-0" name="dis"></div>
-                                            </div>
-                                        </div>
-
-
-                                        
                                         <button type="submit" value="submit" class="btn d-none" id="s_btnn1">submit</button>
                                     </div>
                                 </div>
@@ -83,12 +90,36 @@
 
 </div>
 <script>
+document.addEventListener("DOMContentLoaded", function() {
+    // Remove the 'active' class from the 'des-menu' item
+    const desMenu = document.getElementById("des-menu");
+    const des = document.getElementById("dashboard");
+    if (desMenu) {
+        desMenu.classList.remove("active");
+    }
+    if (des) {
+        des.classList.remove("show");
+        des.classList.remove("active");
+    }
+
+    // Add the 'active' class to the 'cat-menu' item
+    const catMenu = document.getElementById("user-menu");
+    const cat = document.getElementById("apps");
+    if (catMenu) {
+        catMenu.classList.add("active");
+    }
+    if (cat) {
+        cat.classList.add("show");
+        cat.classList.add("active");
+    }
+});
+</script>
+<script>
     // Attach a click event to the first button
     document.getElementById('save_btn1').addEventListener('click', function() {
         // Trigger a click on the second button
         document.getElementById('s_btnn1').click();
     });
-
 </script>
 
 <?= $this->include('admin/common/footer') ?>
